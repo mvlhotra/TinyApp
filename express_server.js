0a1b5877517fc63fb3e97590fac30cbbf6a90ee8
Nik Malhotra <nmal@me.com>
2019-02-12 22:03:26 +0000

    URL Shortening Part 1 Complete

--- express_server.js
+++ express_server.js
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
+
 const express = require('express');
 
 const app = express();
 const PORT = 8080; // default port 8080
+
+const genStr = () => {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+};
 
 app.set("view engine", "ejs");
 
@@ -10,6 +15,10 @@ const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -24,12 +33,22 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
