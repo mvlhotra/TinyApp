18eaf6bc13bcb9eec8836139bd5c66bb08d8b93f
Nik Malhotra <nmal@me.com>
2019-02-13 18:34:28 +0000

    added edit function

--- express_server.js
+++ express_server.js
@@ -53,9 +53,14 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post(`/urls/:shortURL`, (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls")
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
