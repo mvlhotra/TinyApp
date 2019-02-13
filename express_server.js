1d07c2126ddff5c670efdd207c643a6790539439
Nik Malhotra <nmal@me.com>
2019-02-13 20:57:33 +0000

    login/logout features complete

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
 const PORT = 8080; // default port 8080
 
 const genStr = () => {
@@ -10,6 +12,7 @@ const genStr = () => {
 };
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -29,16 +32,22 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 })
 
 app.get(`/urls/:shortURL`, (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -62,6 +71,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
